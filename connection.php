6772a46eec13b6e1a6b1139f8169274d3cdba52f
Kenneth012127 <Johnkennethisuga422@gmail.com>
2025-04-25 20:31:54 -0600

    activity 1 and 2

--- connection.php
+++ connection.php
@@ -1,15 +1,14 @@
-?php
- $servername = "localhost";
- $username = "root";
- $password = "";
- $dbname = "db_crud";
- 
- // Create connection
- $conn = new mysqli($servername, $username, $password, $dbname);
- 
- // Check connection
- if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
- }
- echo "Connected successfully <br/>";
- ?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$database = "db_isuga_project";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+session_start();
+?>
